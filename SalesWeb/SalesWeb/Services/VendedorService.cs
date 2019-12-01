eea09a71a1f1527e8320d5ca510286dc4af0a557
Felipe Neiva <felipec798@gmail.com>
2019-12-01 14:59:30 -0300

    Deletando vendedor

--- SalesWeb/SalesWeb/Services/VendedorService.cs
+++ SalesWeb/SalesWeb/Services/VendedorService.cs
@@ -17,12 +17,23 @@ namespace SalesWeb.Services
 
         public List<Vendedor> FindAll()
         {
-            return _context.Vendedor.ToList(); 
+            return _context.Vendedor.ToList();
         }
 
         public void Insert(Vendedor obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Vendedor FindById(int id)
+        {
+            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            _context.Vendedor.Remove(_context.Vendedor.Find(id));
             _context.SaveChanges();
         }
     }
